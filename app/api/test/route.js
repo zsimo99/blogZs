61260590a94538777f026d478245e5600f32dada
zsimo <mohamedzoraa100@gmail.com>
2023-09-05 13:06:01 +0100

    removePosts

--- app/api/test/route.js
+++
@@ -1,9 +0,0 @@
-import startDB from "@/lib/db"
-import PostModel from "@/models/PostModel"
-import { NextResponse } from "next/server"
-
-export const GET = async (req) => {
-    await startDB()
-    const post = await PostModel.find().populate("comments").populate("likes")
-    return NextResponse.json({ post }, { status: 200 })
-}
